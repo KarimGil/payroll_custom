931fea86d4642ca7fcbae34e596fbfc6e0cf8a28
BuggyDev <43909490+KarimGil@users.noreply.github.com>
2025-04-22 11:55:09 +0500

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Payroll Increment",
+    'author': "My Company",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base', 'hr_payroll', 'hr_work_entry_contract_enterprise'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/hr_yearly_increment_views.xml',
+        # 'views/hr_payslip.xml',
+        'views/hr_contract_views.xml',
+    ],
+    # only loaded in demonstration mode
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ],
+}
+
